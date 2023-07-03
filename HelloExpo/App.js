4229a8c688f1470daa14b7e934a3d24894a32cb6
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-07-03 13:43:47 +0100

    basic styling

--- HelloExpo/App.js
+++ HelloExpo/App.js
@@ -4,17 +4,28 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Max</Text>
-      <StatusBar style="auto" />
+      <Text style={[styles.title, { color: "red", flex: 2 }]}>One</Text>
+      <Text style={[styles.title, { color: "red", flex: 1 }]}>Two</Text>
+      <Text style={[styles.title, { color: "red", flex: 1 }]}>Three</Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1, //will use all available space as orange
+    backgroundColor: "orange",
+    marginTop: 20,
+    // paddingHorizontal: 24,
+    // alignItems: "center",
+    justifyContent: "space-around",
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 600,
+    color: "blue", // overwritten by color red - we shouldn't use in-line styles
+    backgroundColor: "grey",
+    margin: 10,
   },
 });
