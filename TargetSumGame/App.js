2d36bc7c71e61f0b1e4f06a6f8ea852e1a3c53ec
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-07-17 22:38:46 +0100

    Done: The game component with a random target

--- TargetSumGame/App.js
+++ TargetSumGame/App.js
@@ -1,24 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Game from "./src/components/Game/Game.jsx";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! yeah</Text>
-      <Text>W koncu dziala. Wylacz VPN i odpal npx expo start</Text>
-      <Text>
-        Chyba nawet nie trzeba wylaczac vpn, tylko kliknij Play na symulatorze
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Game />
+      {/* <StatusBar style="auto" /> */}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ TargetSumGame/src/components/Game/Game.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
+
+const Game = () => {
+  target = 10 + Math.floor(Math.random() * 40);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.targetNumber}>{this.target}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //stretches across whole screen
+    backgroundColor: "#ddd",
+    paddingTop: 45,
+  },
+  targetNumber: {
+    fontSize: 40,
+    backgroundColor: "#aaa",
+    marginHorizontal: 50,
+    textAlign: "center",
+  },
+});
+
+export default Game;
